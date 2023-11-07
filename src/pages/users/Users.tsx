ccc32b578365db977346babb17abfaef7d30ecd8
Samiul_Karim <samiulkarimprodhan@gmail.com>
2023-11-07 13:22:28 +0600

    added initial DataTable component

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,5 +1,10 @@
 import "./users.scss";
 const Users = () => {
-  return <div className="users">Users</div>;
+  return (
+    <div className="users">
+      <h1 className="heading">Users</h1>
+      <button className="addUser">Add New User</button>
+    </div>
+  );
 };
 export default Users;
