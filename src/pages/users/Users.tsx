b1a3f3547c1a135c0e33dd0a189a5052266aff75
Samiul_Karim <samiulkarimprodhan@gmail.com>
2023-11-07 14:14:39 +0600

    Added initial DataTable component

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,9 +1,11 @@
+import DataTable from "../../Components/DataTable/DataTable";
 import "./users.scss";
 const Users = () => {
   return (
     <div className="users">
       <h1 className="heading">Users</h1>
       <button className="addUser">Add New User</button>
+      <DataTable />
     </div>
   );
 };
